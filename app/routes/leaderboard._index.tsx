00223d0fa79d5369211f2556e252151672412983
Neil Kistner <neil.kistner@gmail.com>
2023-05-14 15:10:06 -0500

    Add cache-control header for leaderboard route

--- app/routes/leaderboard._index.tsx
+++ app/routes/leaderboard._index.tsx
@@ -1,9 +1,20 @@
 import { redirect } from '@remix-run/node'
+import { cacheHeader } from 'pretty-cache-header'
 
 import { today } from '~/lib/date.server'
 
 export function loader() {
   return redirect(`${today}/overview`)
+}
+
+export function headers() {
+  return {
+    'Cache-Control': cacheHeader({
+      public: true,
+      maxAge: '1hour',
+      staleWhileRevalidate: '45minutes',
+    }),
+  }
 }
 
 export default function LeaderboardIndex() {
