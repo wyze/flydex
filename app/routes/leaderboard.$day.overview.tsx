2b6a1bc13cde558fbdaa11dc78c36080a7491118
Neil Kistner <neil.kistner@gmail.com>
2023-05-14 15:09:27 -0500

    Fix dark mode on leaderboard overview

--- app/routes/leaderboard.$day.overview.tsx
+++ app/routes/leaderboard.$day.overview.tsx
@@ -39,7 +39,7 @@ export default function Leaderboard() {
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
             <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-300 dark:divide-gray-700">
+              <table className="min-w-full divide-y divide-red-300 dark:divide-red-800">
                 <thead className="bg-gray-50 dark:bg-gray-800">
                   <tr>
                     <th
@@ -74,11 +74,11 @@ export default function Leaderboard() {
 
                     return (
                       <Fragment key={league}>
-                        <tr className="border-t border-gray-200">
+                        <tr className="border-t border-gray-200 dark:border-gray-700">
                           <th
                             colSpan={4}
                             scope="colgroup"
-                            className="bg-gray-50 py-2 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                            className="bg-gray-50 py-2 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 dark:bg-gray-900 sm:pl-6"
                           >
                             <Link
                               className="text-pink-500 underline"
