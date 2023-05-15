9d3c4482f471878497b59d39fc7323cfcfb6e4e1
Neil Kistner <neil.kistner@gmail.com>
2023-05-14 19:58:10 -0500

    Move caching headers for leaderboard into loader response

--- app/routes/leaderboard._index.tsx
+++ app/routes/leaderboard._index.tsx
@@ -4,17 +4,15 @@ import { cacheHeader } from 'pretty-cache-header'
 import { today } from '~/lib/date.server'
 
 export function loader() {
-  return redirect(`${today}/overview`)
-}
-
-export function headers() {
-  return {
-    'Cache-Control': cacheHeader({
-      public: true,
-      maxAge: '1hour',
-      staleWhileRevalidate: '45minutes',
-    }),
-  }
+  return redirect(`${today}/overview`, {
+    headers: {
+      'Cache-Control': cacheHeader({
+        public: true,
+        maxAge: '30minutes',
+        staleWhileRevalidate: '20minutes',
+      }),
+    },
+  })
 }
 
 export default function LeaderboardIndex() {
